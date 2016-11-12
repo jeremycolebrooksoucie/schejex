42d9cdcb650b29fa87c47832c52895c537c49de1
Jeremy <jeremy.colebrooksoucie@gmail.com>
2016-11-12 14:43:46 -0500

    Reworked request as gen server

--- lib/request.ex
+++ lib/request.ex
@@ -1,22 +1,56 @@
 defmodule Request do
-    def init(dict) do
-        {:ok, pid} = Agent.start(fn -> dict end)
+    use GenServer
+    
+    @doc """
+    Creates a new Request with initial_state and forwards it to
+        the registered scheduler process
+    """
+    def start(initial_state) do
+        {:ok, pid} = GenServer.start(__MODULE__, initial_state)
         pid
-    end
-
-    # agent PID, key -> value at key
-    def get(request_agent, key) do
-        Agent.get(request_agent, 
-                  fn dict -> Map.get(dict, key) end)
-                  #&(Map.get(&1, key)))
+        #forward pid to registered scheduler 
 
     end
 
-    # agent PID, key, value -> :ok
-    def set(request_agent, key, value) do 
-        Agent.update(request_agent,
-                     fn dict -> %{dict | key => value} end)
-                     #&(%{&1 | key => value})) 
-        :ok
+    def init(initial_state) do
+        # register self
+        {:ok, initial_state}
+    end
+
+    def handle_call({:get_state}, from, state) do
+        {:reply, state, state}  
+    end
+
+    # def handle_cast(request, state) do
+        
+    # end
+
+
+    def get_state(request_pid) do
+        GenServer.call(request_pid, {:get_state})
     end
 end
+
+
+# defmodule Request do
+#     def init(dict) do
+#         {:ok, pid} = Agent.start(fn -> dict end)
+#         pid
+#     end
+
+#     # agent PID, key -> value at key
+#     def get(request_agent, key) do
+#         Agent.get(request_agent, 
+#                   fn dict -> Map.get(dict, key) end)
+#                   #&(Map.get(&1, key)))
+
+#     end
+
+#     # agent PID, key, value -> :ok
+#     def set(request_agent, key, value) do 
+#         Agent.update(request_agent,
+#                      fn dict -> %{dict | key => value} end)
+#                      #&(%{&1 | key => value})) 
+#         :ok
+#     end
+# end
