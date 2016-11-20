744113c610a565a97ea5d142e007374d0af38e82
Jeremy <jeremy.colebrooksoucie@gmail.com>
2016-11-20 15:46:31 -0500

    Added Kalina's code to user because she did it under Justin's name

--- lib/user.ex
+++ lib/user.ex
@@ -1,7 +1,7 @@
 defmodule User do
 	def assign_consumer(consumers, request_pid) do
         {min_pid, size} = Map.to_list(consumers) 
-            |> Enum.min_by(fn({_pid, num}) -> num end)
+            > Enum.min_by(fn({_pid, num}) -> num end)
 		IO.puts("Assigning consumer")
         IO.inspect(request_pid)
         min_pid
@@ -10,4 +10,10 @@ defmodule User do
     def update_request(request_state, {consumer_state, _}, _update_data) do 
         consumer_state + request_state
     end
+
+
+    def interpret_request_update(update, consumer_state, request_state) do
+        IO.puts("user got a request update!")
+    end
+
 end
